2314fcbd5a9389343cafd484308873be78df2489
Trodis <galerian@gmx.net>
2013-09-02 22:55:42 +0200

    test

---
+++ src/main/java/com/example/aal_app/tre.java
@@ -0,0 +1,11 @@
+package com.example.aal_app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: trodis
+ * Date: 02.09.13
+ * Time: 22:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class tre {
+}
